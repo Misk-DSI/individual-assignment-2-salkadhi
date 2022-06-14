e108e8294cc9e8e3527605f48ad67f9a4503fa0d
salkadhi <ssalkadhi@gmail.com>
2022-06-14 21:07:36 +0300

    remove unhelpful code from eda

--- src/wine_EDA.R
+++ src/wine_EDA.R
@@ -1,120 +1,106 @@
-# install.packages("tidyverse")
+#Establish root dir ####
+#install.packages("here")
 library(tidyverse)
+here::i_am("src/wine_EDA.R")
+#Install packages ####
+# install.packages("reactable")
+# install.packages("tidyverse")
+# install.packages("janitor")
+#install.packages("skimr")
+#install.packages("dlookr")
+#install.packages("flextable")
+# install.packages("ggstatsplot")
+#install.packages("PerformanceAnalytics")
+#install.packages("sm")
+#install.packages("flexdashboard")
+# install.packages("gt")
+# install.packages("DT")
+#install.packages("moments")
+#library(here) #
+#library(reactable)#y
+#library(janitor)#y
+library(skimr)
+#library(DataExplorer) #y
+library(dlookr) #y
+#library(flextable) #y
+library(moments)
+#library(ggstatsplot)#y
+library(purrr)
+library(PerformanceAnalytics)
+library(sm)
+library(flexdashboard)
+library(gt)
+library(DT)
+
+#Load and clean dataset
 getwd()
 wine <- read_csv("data/winequality-red.csv")
-# install.packages("janitor")
-library(janitor)
-wine
-#add underscore to column names
-wine <- clean_names(wine)
-str(wine)
-glimpse(wine)
-summary(wine)
-dim(wine)
-length(wine)
-names(wine)
-wine$quality#change p_h to ph
-colnames(wine)[colnames(wine) == 'p_h'] <- 'ph'
-names(wine)
-summary(wine$quality)
-table(wine$quality)
-#No missing data
-sum(is.na(wine))
-#install.packages("skimr")
-library(skimr)
-skim(wine)
-#wine quality plot ####
-hist(wine$quality)
-barplot(table(wine$quality))
-pie(table(wine$quality))
-#wine zscore ####
-wine_zscores = scale(wine)
-view(wine_zscores)
-library(DataExplorer)
-#install.packages("dlookr")
-library(dlookr)
-describe(wine)
-#install.packages("flextable")
-library(flextable)
-# Beautified tables ####
-#one
-describe(wine) %>% flextable()
-#two
-wine %>% 
-  diagnose_numeric() %>% 
-  flextable()
-#Check skewness of all variables ####
-wine %>% 
-  plot_density()
-#install.packages("moments")
-#Skewness and kurtosis ####
-library(moments)
-sapply(wine, skewness)
-sapply(wine, agostino.test)
-sapply(wine, anscombe.test)
-# QQ Plots ####
-plot_qq(wine, by="quality")
-#Normality test ####
-#The null hypothesis of these tests is that “sample distribution is normal”. 
-#If the test is significant, the distribution is non-normal.
-normality(wine) %>%
+wine <- janitor::clean_names(wine) #remove spaces
+colnames(wine)[colnames(wine) == 'p_h'] <- 'ph'#change p_h to ph
+
+
+
+#Explore data ####
+sum(is.na(wine))#No missing data
+str(wine)#all variables are type double
+#glimpse(wine)
+#summary(wine)
+#dim(wine)
+#length(wine)
+#names(wine)
+wine$quality #wine quality seems to be whole numbers
+#summary(wine$quality)
+#table(wine$quality)
+#skim(wine)
+
+#FIRST PART static table ####
+dlookr::describe(wine) %>% 
+  flextable::flextable(
+    col_keys =
+      c("described_variables","mean", 
+        "p00", "p50", "p100")) %>%
+          flextable::colformat_double(digits = 2) %>% 
+              flextable::bold(part="header") %>% 
+                flextable::border_inner_h() 
+#SECOND PART ONE interactive table ####
+ggstatsplot::ggcorrmat(data = wine, type = "np", output = "dataframe", theme=) %>% 
   mutate_if(is.numeric, ~round(.,2)) %>% 
-  flextable()
+  reactable::reactable()
+#another possible interactive table
+winecorr <- compare_numeric(wine)
+winecorr$correlation
+winecorr$linear %>% mutate_if(is.numeric, ~round(.,2)) %>%
+  flextable::flextable()  
+
+#SECOND PART TWO data vis ####
+wine %>% 
+  DataExplorer::plot_density() #visualisation of skewness
+
+#plot_correlate(wine, method = "kendall") #this is from dlookr will not work unless I load the whole package
+#OR
+ggstatsplot::ggcorrmat(data = wine) #includes significance
+#Drill into alcohol vs quality correlation ####
+#ggstatsplot::ggscatterstats(data = wine, x=alcohol, y=quality, type="np")##useless plot
+
+#wine quality plot 
+#hist(wine$quality) #bars are too spread out
+barplot(table(wine$quality), xlab = "Wine Quality", ylab = "Frequency")
+pie(table(wine$quality), radius = 1, main = "Pie Chart of Wine Quality")
+
+#Drill into alcohol vs quality correlation 
+#ggstatsplot::ggscatterstats(data = wine, x=alcohol, y=quality, type="np")#useless
+#OR 
+sm::sm.density.compare(wine$alcohol, wine$quality, xlab="Alcohol Content")
+quality_legend = factor(wine$quality, labels=c("3","4","5","6","7","8"))
+colfill <- c(2:(2+length(levels(quality_legend))))
+legend("topright", levels(quality_legend), fill=colfill, title = "Quality")
+
+#Skewness and kurtosis 
+kableExtra::kable(wine) %>% sapply(skewness) %>% kableExtra::kable_styling()
+sapply(wine, skewness) #quality is not skewed, alcohol is moderately skewed.  
 
 wine %>% 
   normality()
 
-#Box plots of quality vs all variable ####
-plot_boxplot(wine, by = "quality")
-# install.packages("ggstatsplot")
-library(ggstatsplot)
-library(purrr)
-#A bunch of plots ####
-plot_list <- pmap(.l = list(data = list(wine), x="quality", y=list("chlorides", "ph")), .f=ggbetweenstats)
-plot_list[[1]]
-# Correlations ####
-#2 graphs and beautified table
-correlate(wine, quality)
-library(dlookr)
-plot_correlate(wine)
-plot_correlate(wine, method = "kendall")
-ggcorrmat(data = wine)
-ggcorrmat(data = wine, type = "np", output = "dataframe") %>% mutate_if(is.numeric, ~round(.,2)) %>% 
-  flextable()
-#Drill into alcohol vs quality correlation ####
-ggscatterstats(data = wine, x=alcohol, y=quality, type="np")
-#install.packages("PerformanceAnalytics")
-library(PerformanceAnalytics)
-# Correlations. Another Method ####
-#DON'T RERUN THIS METOD as is it will break r
-#chart.Correlation(wine, method="kendall")
 
-winecorr <- compare_numeric(wine)
-winecorr$correlation
-#yet another correlation table
-winecorr$linear %>% mutate_if(is.numeric, ~round(.,2)) %>% flextable()
-#More correlation plots ####
-plot(winecorr)  
 
-#Using ggplot ####
-ggplot(wine, aes(alcohol, quality)) +
-  geom_point()+
-  geom_smooth()
-#Density plot of alcohol of vs quality ####
-#install.packages("sm")
-library(sm)
-sm.density.compare(wine$alcohol, wine$quality, xlab="Alcohol Content")
-quality_legend = factor(wine$quality, labels=c("3","4","5","6","7","8"))
-colfill <- c(2:(2+length(levels(quality_legend))))
-legend("topright", levels(quality_legend), fill=colfill, title = "Quality")
-scatter.smooth(wine$alcohol, wine$quality)
-#install.packages("here")
-library(here)
-i_am("src/wine_EDA.R")
-here()
-#install.packages("flexdashboard")
-# install.packages("gt")
-# install.packages("DT")
-library(flexdashboard)
-library(gt)
-library(DT)
