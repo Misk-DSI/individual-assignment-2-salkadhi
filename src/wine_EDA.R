052343f60c0f04a63bb8b8f77744046c2564b37e
salkadhi <ssalkadhi@gmail.com>
2022-06-15 22:45:19 +0300

    early rmd draft

--- src/wine_EDA.R
+++ src/wine_EDA.R
@@ -34,43 +34,41 @@ library(DT)
 
 #Load and clean dataset
 getwd()
-wine <- read_csv("data/winequality-red.csv")
+wine <- readr::read_csv("data/winequality-red.csv")
 wine <- janitor::clean_names(wine) #remove spaces
 colnames(wine)[colnames(wine) == 'p_h'] <- 'ph'#change p_h to ph
-
-
-
-#Explore data ####
-sum(is.na(wine))#No missing data
-str(wine)#all variables are type double
-#glimpse(wine)
-#summary(wine)
-#dim(wine)
-#length(wine)
-#names(wine)
+head(wine)#all variables are type double
+sum(is.na(wine))()#No missing data
 wine$quality #wine quality seems to be whole numbers
-#summary(wine$quality)
-#table(wine$quality)
-#skim(wine)
+#Explore data ####
+
+str(wine)
+glimpse(wine)
+summary(wine)
+dim(wine)
+length(wine)
+names(wine)
+summary(wine$quality)
+table(wine$quality) 
+skimr::skim(wine)
 
 #FIRST PART static table ####
-dlookr::describe(wine) %>% 
-  flextable::flextable(
-    col_keys =
-      c("described_variables","mean", 
-        "p00", "p50", "p100")) %>%
-          flextable::colformat_double(digits = 2) %>% 
-              flextable::bold(part="header") %>% 
-                flextable::border_inner_h() 
-#SECOND PART ONE interactive table ####
-ggstatsplot::ggcorrmat(data = wine, type = "np", output = "dataframe", theme=) %>% 
+ 
+dlookr::describe(wine) %>%
   mutate_if(is.numeric, ~round(.,2)) %>% 
-  reactable::reactable()
+  gt::gt()
+
+#SECOND PART ONE interactive table ####
+ggstatsplot::ggcorrmat(data = wine, type = "np", output = "dataframe", digits=2 ) %>%
+  select(-"method", -"n.obs", "conf.level", "statistic") %>%
+  mutate_if(is.numeric, ~round(.,2)) %>% 
+  DT::datatable()
+
 #another possible interactive table
-winecorr <- compare_numeric(wine)
-winecorr$correlation
-winecorr$linear %>% mutate_if(is.numeric, ~round(.,2)) %>%
-  flextable::flextable()  
+# winecorr <- dlookr::compare_numeric(wine)
+# winecorr$correlation
+# winecorr$linear %>% mutate_if(is.numeric, ~round(.,2)) %>%
+#   flextable::flextable()  
 
 #SECOND PART TWO data vis ####
 wine %>% 
@@ -78,29 +76,29 @@ wine %>%
 
 #plot_correlate(wine, method = "kendall") #this is from dlookr will not work unless I load the whole package
 #OR
-ggstatsplot::ggcorrmat(data = wine) #includes significance
+ggstatsplot::ggcorrmat(data = wine, type = "nonparameteric", title = "Correlation matrix plot", legend.title.margin = FALSE ) #includes significance
 #Drill into alcohol vs quality correlation ####
-#ggstatsplot::ggscatterstats(data = wine, x=alcohol, y=quality, type="np")##useless plot
-
+# ggstatsplot::ggscatterstats(data = wine, x=alcohol, y=quality, type="np")##useless plot
+head(wine)
 #wine quality plot 
 #hist(wine$quality) #bars are too spread out
-barplot(table(wine$quality), xlab = "Wine Quality", ylab = "Frequency")
-pie(table(wine$quality), radius = 1, main = "Pie Chart of Wine Quality")
+# barplot(table(wine$quality), xlab = "Wine Quality", ylab = "Frequency")
+# pie(table(wine$quality), radius = 1, main = "Pie Chart of Wine Quality")
 
 #Drill into alcohol vs quality correlation 
 #ggstatsplot::ggscatterstats(data = wine, x=alcohol, y=quality, type="np")#useless
 #OR 
-sm::sm.density.compare(wine$alcohol, wine$quality, xlab="Alcohol Content")
+sm::sm.density.compare(wine$alcohol, wine$quality, xlab="Alcohol Content", title("Wine Density Comparison Plot"))
 quality_legend = factor(wine$quality, labels=c("3","4","5","6","7","8"))
 colfill <- c(2:(2+length(levels(quality_legend))))
 legend("topright", levels(quality_legend), fill=colfill, title = "Quality")
 
 #Skewness and kurtosis 
-kableExtra::kable(wine) %>% sapply(skewness) %>% kableExtra::kable_styling()
-sapply(wine, skewness) #quality is not skewed, alcohol is moderately skewed.  
+sapply(wine, dlookr::skewness) %>% data.frame
+#quality is not skewed, alcohol is moderately skewed.  
 
 wine %>% 
-  normality()
+  dlookr::normality()
 
-
+getwd()
 
